dce6455f4b95cb4a2e2f163cd377d0cbf6bbb189
zonesama <trannhatthanh31@gmail.com>
2019-10-23 22:59:49 +0700

    First Commit.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        Shape s1= new Shape();
+        System.out.println(s1.toString());
+        Shape s2 = new Shape("yellow",false);
+        System.out.println(s2.toString());
+
         Triangle t1 = new Triangle();
         System.out.println(t1.toString());
         Triangle t2 = new Triangle(6.0,6.0,6.0);
